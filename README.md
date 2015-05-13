f7a2346a186d46fcc63830db140829c57a2b5180
papix <mail@papix.net>
2015-05-13 19:28:07 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WebService::Reactio - It's new $module
+
+# SYNOPSIS
+
+    use WebService::Reactio;
+
+# DESCRIPTION
+
+WebService::Reactio is ...
+
+# LICENSE
+
+Copyright (C) papix.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+papix <mail@papix.net>
---
+++ lib/WebService/Reactio.pm
@@ -0,0 +1,39 @@
+package WebService::Reactio;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::Reactio - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebService::Reactio;
+
+=head1 DESCRIPTION
+
+WebService::Reactio is ...
+
+=head1 LICENSE
+
+Copyright (C) papix.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+papix E<lt>mail@papix.netE<gt>
+
+=cut
+
